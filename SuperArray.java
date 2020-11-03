1f3bf29fa0dfdc33d484cd2a6fcee5e2fbe4fc8a
DickersonAJ <adickerson10@stuy.edu>
2020-11-03 09:38:45 -0500

    instance variables, constructor and size created and tested (I know the constructor has issues with dat = new String[10])

---
+++ SuperArray.java
@@ -0,0 +1,35 @@
+public class SuperArray {
+  private String[] data;
+  private int size;
+
+  public void SuperArray() {
+    data = new String[10];
+    size = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  /*public boolean add(String element) {
+    if (size < data.length) {
+      System.out.println(data);
+      System.out.println(size);
+      data[size] = element;
+      size+=1;
+      return true;
+    }
+    return false;
+  }
+
+  public String get(int index) {
+    return data[index];
+  }
+
+  public String set(int index, String element) {
+    String str = data[index];
+    data[index] = element;
+    return str;
+  }*/
+
+}
---
+++ SuperArrayTester.java
@@ -0,0 +1,14 @@
+public class SuperArrayTester {
+
+  public static void main(String[] args) {
+    SuperArray words = new SuperArray();
+    System.out.println( words.size() );
+    /*words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    for(int i = 0; i < words.size(); i++){
+      System.out.println( words.get(i) );
+    }*/
+  }
+
+}
