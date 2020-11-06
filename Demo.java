af721688af2ccdea75142271b4fd177d7327537e
DickersonAJ <adickerson10@stuy.edu>
2020-11-06 12:11:42 -0500

    added removeDuplicates and findOVerlap

---
+++ Demo.java
@@ -0,0 +1,56 @@
+public class Demo{
+
+  public static void removeDuplicates(SuperArray s) {
+    for (int i = s.size() - 1; i > 0; i--) {
+      if (s.indexOf(s.get(i)) != i) {
+        s.remove(i);
+      }
+      /*for (int j = i - 1; j >= 0; j--) {
+        if (s.get(i).equals(s.get(j))) {
+          s.remove(i);
+          j = -1;
+        }
+      }*/
+    }
+  }
+
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+    SuperArray o = new SuperArray();
+    for (int i = 0; i < a.size(); i++) {
+      if (b.contains(a.get(i))) {
+        o.add(a.get(i));
+      }
+    }
+    removeDuplicates(o);
+    return o;
+  }
+
+  public static void main(String[]args){
+    SuperArray words = new SuperArray();
+    //grouped to save vertical space
+    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+    words.add("una");    words.add("ebi");     words.add("toro");
+
+    System.out.println(words);
+    removeDuplicates(words);
+    System.out.println(words);
+
+    SuperArray a = new SuperArray(6);
+    a.add("9");
+    a.add("2");
+    a.add("1");
+    a.add("2");
+    a.add("4");
+    a.add("3");
+    SuperArray b = new SuperArray(7);
+    b.add("0");
+    b.add("0");
+    b.add("0");
+    b.add("4");
+    b.add("2");
+    b.add("2");
+    b.add("9");
+    System.out.println(findOverlap(a, b));
+  }
+}
